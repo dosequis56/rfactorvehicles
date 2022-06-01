8b67b61353cf098d7b3eae21b37230a119ed0b24
dosequis56 <thansen56@outlook.com>
2022-06-01 07:57:27 -0400

    Update car build

--- ASMG_F1_2021/build.bat
+++ ASMG_F1_2021/build.bat
@@ -1,7 +1,9 @@
 set PATH=%PATH%;"C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Bin64"
 set MODDEV="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev"
-set COMPILER="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\bin\MASCompiler.exe"
+set COMPILER="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\Tools\MASCompiler\bin\Release\MASCompiler.exe"
+set PACKAGER="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\Tools\AssetPackager\bin\Release\AssetPackager.exe"
 %COMPILER% %MODDEV%\vehicles\ASMG_F1_2021\ASMG_F1_2021.rfbuild %1
+%PACKAGER% build bin\ASMG_F1_2021.zip %MODDEV%\vehicles\ASMG_F1_2021\tracks
 echo Build complete
 
 
--- ASMG_F1_2021/clean.bat
+++ ASMG_F1_2021/clean.bat
@@ -1,6 +1,8 @@
 set PATH=%PATH%;"C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Bin64"
 set MODDEV="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev"
 del /F %MODDEV%\vehicles\ASMG_F1_2021\bin\*.mas
+del /F %MODDEV%\vehicles\ASMG_F1_2021\bin\*.rfcmp
+del /F %MODDEV%\vehicles\ASMG_F1_2021\bin\*.zip
 rem del /F %MODDEV%\vehicles\ASMG_F1_2021\bin\hash\*.hash
 echo "clean complete"
 
