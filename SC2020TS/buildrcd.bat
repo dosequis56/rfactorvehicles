e7e829439e1b28e7369cab3aa94c4ff16b17da13
dosequis56 <thansen56@outlook.com>
2024-03-07 19:59:02 -0500

    Updates to truck series for Kentucky speedway

--- SC2020TS/buildrcd.bat
+++ SC2020TS/buildrcd.bat
@@ -5,3 +5,4 @@ set COMPILER="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\Too
 set PACKAGER="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\Tools\AssetPackager\bin\Release\AssetPackager.exe"
 set RCD="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\Tools\RCDTool\bin\x64\Release\net6.0\RCDTool.exe"
 %RCD% writercd %MODDEV%\Vehicles\SC2020TS\%BUILD_PROJECT_NAME%.xlsx -output=%BUILD_PROJECT_NAME%.rcd -class=%BUILD_PROJECT_NAME%
+copy %BUILD_PROJECT_NAME%.rcd %MODDEV%\talent
