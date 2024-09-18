0d50720d0177d9a64059376a26492c8375a82723
thansen56 <thansen56@outlook.com>
2024-09-18 19:58:46 -0400

    Talent file updates for Bristol, Dover, Martinsville and Richmond.  Also include project files.

---
+++ SCNG2023/buildrcd.bat
@@ -0,0 +1,8 @@
+set BUILD_PROJECT_NAME=SCNG23
+set PATH=%PATH%;"C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Bin64"
+set MODDEV="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev"
+set COMPILER="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\Tools\MASCompiler\bin\Release\MASCompiler.exe"
+set PACKAGER="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\Tools\AssetPackager\bin\Release\AssetPackager.exe"
+set RCD="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\Tools\RCDTool\bin\x64\Release\net6.0\RCDTool.exe"
+%RCD% writercd %MODDEV%\Vehicles\SCNG2023\%BUILD_PROJECT_NAME%.xlsx -output=%BUILD_PROJECT_NAME%.rcd -class=%BUILD_PROJECT_NAME%
+copy %BUILD_PROJECT_NAME%.rcd %MODDEV%\talent
