128dfb39d2eb627a8720b7ba7a819a9b1a819264
dosequis56 <thansen56@outlook.com>
2025-04-22 18:22:31 -0400

    Next gen RCD updates

--- SCNG2023/build.bat
+++ SCNG2023/build.bat
@@ -2,7 +2,7 @@ set BUILD_PROJECT_NAME=SCNG2023
 set PATH=%PATH%;"C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Bin64"
 set MODDEV="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev"
 set COMPILER="C:\Program Files\Saberace\RFactorTools\RFBuild.exe"
-rem set COMPILER="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\Tools\RFBuild\bin\Release\net8.0-windows8.0\RFBuild.exe"
+set COMPILER="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\Tools\RFBuild\bin\Release\net8.0-windows8.0\RFBuild.exe"
 call ..\version.bat
 del %BUILD_TARGET_PATH%\RFACTOR\%BUILD_PROJECT_NAME%\*.rfcmp
 %COMPILER% %MODDEV%\vehicles\SCNG2023\SCNG2023.rfbuild -r -source=Vehicles\%BUILD_PROJECT_NAME% -target=%BUILD_TARGET_PATH%\RFACTOR\%BUILD_PROJECT_NAME% -v=%BUILD_TARGET_VERSION%
--- SCNG2023/buildrcd.bat
+++ SCNG2023/buildrcd.bat
@@ -3,6 +3,6 @@ set PATH=%PATH%;"C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\Bin64"
 set MODDEV="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev"
 set COMPILER="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\Tools\MASCompiler\bin\Release\MASCompiler.exe"
 set PACKAGER="C:\Program Files (x86)\Steam\steamapps\common\rFactor 2\ModDev\Tools\AssetPackager\bin\Release\AssetPackager.exe"
-set RCD="C:\Program Files\Saberace\RFactorTools\RCDTool.exe"
-%RCD% writercd %MODDEV%\Vehicles\SCNG2023\%BUILD_PROJECT_NAME%.xlsx -output=%BUILD_PROJECT_NAME%.rcd -class=%BUILD_PROJECT_NAME%
-copy %BUILD_PROJECT_NAME%.rcd %MODDEV%\talent
+set RCD="D:\Games\steamapps\common\rFactor 2\ModDev\Tools\RCDTool\bin\Release\net8.0-windows8.0\RCDTool.exe"
+%RCD% writercd %MODDEV%\Vehicles\SCNG2023\%BUILD_PROJECT_NAME%.xlsx -output=%BUILD_PROJECT_NAME%.rcd,%BUILD_PROJECT_NAME%-AI.rcd -class=%BUILD_PROJECT_NAME%,%BUILD_PROJECT_NAME%-AI
+
